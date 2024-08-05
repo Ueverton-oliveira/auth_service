7f0efa829395e9cb882edbf3127e802fade81262
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-05 16:09:16 -0400

    feat: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    post 'register', to: 'auth#register'
-    post 'login', to: 'auth#login'
-    get 'validate_token', to: 'auth#validate_token'
+    namespace :v1 do
+      post 'register', to: 'auth#register'
+      post 'login', to: 'auth#login'
+      get 'validate_token', to: 'auth#validate_token'
+    end
   end
 end
